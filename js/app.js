9a1d927e9cae6e4e7696b07908ba583f1db63ed9
Matias Gomez <jmgametal@gmail.com>
2020-04-10 18:58:15 -0300

    Seccion 2: JavaScript - Repaso / Clase 17: Objetos en JavaScript - Object Constructor

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Object Literal
 const persona = {
     nombre: 'Matias',
     edad: 33,
@@ -8,3 +9,13 @@ console.log(persona);
 console.log(persona.nombre);
 console.log(persona.edad);
 console.log(persona['profesion']);
+
+// Object Constructor
+function Tarea(nombre, urgencia) {
+    this.nombre = nombre;
+    this.urgencia = urgencia;
+}
+
+const tarea1 = new Tarea('Aprender React', 'Urgente');
+
+console.log(tarea1);
