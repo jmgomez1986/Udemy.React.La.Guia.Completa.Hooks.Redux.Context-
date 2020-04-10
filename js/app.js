df50cbc199e4440288692e4886096c65d925ef06
Matias Gomez <jmgametal@gmail.com>
2020-04-10 19:29:11 -0300

    Seccion 2: JavaScript - Repaso / Clase 18: Prototypes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,14 @@
-// Object Literal
-const persona = {
-    nombre: 'Matias',
-    edad: 33,
-    profesion: 'Desarrollador Web'
-};
-
-console.log(persona);
-console.log(persona.nombre);
-console.log(persona.edad);
-console.log(persona['profesion']);
-
 // Object Constructor
 function Tarea(nombre, urgencia) {
     this.nombre = nombre;
     this.urgencia = urgencia;
 }
 
+Tarea.prototype.mostrarInformacionTarea = function() {
+    return `La tarea ${this.nombre} tiene una prioridad ${this.urgencia}`;
+}
+
 const tarea1 = new Tarea('Aprender React', 'Urgente');
 
 console.log(tarea1);
+console.log(tarea1.mostrarInformacionTarea());
