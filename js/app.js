d9af0319cf41204170b8d70e53796b5c8c8aacb7
Matias Gomez <jmgametal@gmail.com>
2020-04-12 18:52:55 -0300

    Seccion 2: JavaScript - Repaso / Clase 25: Promises

--- js/app.js
+++ js/app.js
@@ -1,31 +1,17 @@
-let personas = [
-  { nombre: 'Matias', edad: 33, aprendiendo: 'React' },
-  { nombre: 'Analia', edad: 18, aprendiendo: 'Angular' },
-  { nombre: 'Carlos', edad: 21, aprendiendo: 'PHP' },
-  { nombre: 'Alejandra', edad: 35, aprendiendo: 'Python' },
-  { nombre: 'Martin', edad: 30, aprendiendo: 'JavaScipt' },
-  { nombre: 'Miguel', edad: 27, aprendiendo: 'Go' },
-];
+const aplicarDescuento = new Promise((resolve, reject) => {
+    setTimeout( () => {
+        let descuento = true;
 
-console.log('Personas: ', personas);
-
-// Usando Filter: mayores a 28 años
-const mayores = personas.filter(persona => {
-    return persona.edad > 28;
+        if(descuento) {
+            resolve('Descuento aplicado!');
+        } else {
+            reject('No se pudo aplicar el decuento');
+        }
+    }, 3000);
 });
 
-console.log('Mayores a 28 años: ', mayores);
-
-// Usando Find: obtener datos de un elemento particular del arreglo
-const alejandra = personas.find(persona => {
-    return persona.nombre === 'Alejandra';
+aplicarDescuento.then(resultado => {
+    console.log(resultado);
+}).catch(error => {
+    console.log(error);
 });
-
-console.log(`Alejandra esta aprendiendo ${alejandra.aprendiendo}`);
-
-// Usando Reduce: obener las edades totales
-const total = personas.reduce((edadTotal, persona) => {
-    return edadTotal + persona.edad;
-}, 0);
-
-console.log('Promedio de edades: ', (total / personas.length));
