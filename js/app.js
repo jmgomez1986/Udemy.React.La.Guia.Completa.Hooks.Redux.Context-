a8b153598d855008e54c7ff38a6d4ed178235656
Matias Gomez <jmgametal@gmail.com>
2020-04-12 17:37:21 -0300

    Seccion 2: JavaScript - Repaso / Clase 22: Arreglos, map y Object.keys

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
-// Forma anterior
+const carrito = ["Producto 1", "Producto 2", "Producto 3"];
+
+console.log(carrito);
+
+// Array
+
+carrito.forEach((producto) => {
+  console.log(`(Usando Array) El producto es ${producto}`);
+});
+
+// Map
+
+const carrito2 = carrito.map((producto) => {
+  return `El producto es ${producto}`;
+});
+
+console.log("Usando Map: ", carrito2);
+
+// Object key
+
 const persona01 = {
-    nombre: 'Matias',
-    trabajo: 'Desarrollador Web',
-    edad: 500,
-    musicaRock: true,
-    mostrarInformacion: function() {
-        console.log(
-          `Forma anterior: ${this.nombre} es ${this.trabajo} y su edad es ${this.edad}`
-        );
-    }
-}
-
-persona01.mostrarInformacion();
-
-// Forma nueva
-
-const persona02 = {
   nombre: "Matias",
   trabajo: "Desarrollador Web",
   edad: 500,
-  musicaRock: true,
-  mostrarInformacion() {
-    console.log(
-      `Forma nueva: ${this.nombre} es ${this.trabajo} y su edad es ${this.edad}`
-    );
-  },
 };
 
-persona02.mostrarInformacion();
+const { nombre } = persona01;
+console.log(nombre);
+
+console.log(Object.keys(persona01));
