c6fbfae1c516ac253215841ca8e8fbba929c2171
Matias Gomez <jmgametal@gmail.com>
2020-04-12 19:26:27 -0300

    Seccion 2: JavaScript - Repaso / Clase 26: Promises con Ajax

--- js/app.js
+++ js/app.js
@@ -1,17 +1,52 @@
-const aplicarDescuento = new Promise((resolve, reject) => {
-    setTimeout( () => {
-        let descuento = true;
+// Hacer la petición con ajax
+const descargarUsuarios = (cantidad) =>
+  new Promise(async (resolve, reject) => {
+    const api = `https://randomuser.me/api/?results=${cantidad}&nat=us`;
 
-        if(descuento) {
-            resolve('Descuento aplicado!');
-        } else {
-            reject('No se pudo aplicar el decuento');
-        }
-    }, 3000);
-});
+    const xhr = new XMLHttpRequest();
 
-aplicarDescuento.then(resultado => {
-    console.log(resultado);
-}).catch(error => {
-    console.log(error);
-});
+    xhr.open("GET", api, true);
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(JSON.parse(xhr.responseText).results);
+      } else {
+        reject(Error(xhr.statusText));
+      }
+    };
+
+    xhr.onerror = (error) => reject(error); // opcional
+
+    xhr.send();
+  });
+
+descargarUsuarios(20)
+    .then(
+        (miembros) => console.log(miembros),
+        (error) => console.error(new Error('Hubo un error: ' + error))
+    );
+
+// Hacer la petición con fetch
+// const downloadUser = (cantidad) =>
+//   new Promise(async (resolve, reject) => {
+//     const api = `https://randomuser.me/api/?results=${cantidad}&nat=us`;
+
+//     try {
+//       const dataAjax = await fetch(api);
+//       const results = await dataAjax.json();
+//       resolve(results);
+//     } catch (error) {
+//       reject(error);
+//     }
+//   });
+
+// async function called() {
+//   try {
+//     const data = await downloadUser(20);
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// called();
