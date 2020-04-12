b8fd424f71887a1714fb69cdf0dfd52eb8a8347d
Matias Gomez <jmgametal@gmail.com>
2020-04-12 18:35:05 -0300

    Seccion 2: JavaScript - Repaso / Clase 24: Filter .find y .reduce y métodos para arreglos

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
-let lenguajes = ['Javacript', 'PHP', 'Python'];
-let frameworks = ['ReactJS', 'Laravel', 'Django'];
+let personas = [
+  { nombre: 'Matias', edad: 33, aprendiendo: 'React' },
+  { nombre: 'Analia', edad: 18, aprendiendo: 'Angular' },
+  { nombre: 'Carlos', edad: 21, aprendiendo: 'PHP' },
+  { nombre: 'Alejandra', edad: 35, aprendiendo: 'Python' },
+  { nombre: 'Martin', edad: 30, aprendiendo: 'JavaScipt' },
+  { nombre: 'Miguel', edad: 27, aprendiendo: 'Go' },
+];
 
-// Unir arreglos en uno
+console.log('Personas: ', personas);
 
-let combinacion01 = lenguajes.concat(frameworks);
+// Usando Filter: mayores a 28 años
+const mayores = personas.filter(persona => {
+    return persona.edad > 28;
+});
 
-console.log('Union forma anterior: ', combinacion01);
+console.log('Mayores a 28 años: ', mayores);
 
-let combinacion02 = [...lenguajes, ...frameworks];
+// Usando Find: obtener datos de un elemento particular del arreglo
+const alejandra = personas.find(persona => {
+    return persona.nombre === 'Alejandra';
+});
 
-console.log('Union forma nueva: ', combinacion02);
+console.log(`Alejandra esta aprendiendo ${alejandra.aprendiendo}`);
 
+// Usando Reduce: obener las edades totales
+const total = personas.reduce((edadTotal, persona) => {
+    return edadTotal + persona.edad;
+}, 0);
+
+console.log('Promedio de edades: ', (total / personas.length));
