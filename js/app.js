2409715e53036a3283136c2f30744c4b022673a0
Matias Gomez <jmgametal@gmail.com>
2020-04-19 15:28:48 -0300

    Seccion 2: JavaScript - Repaso / Clase 29: Programación Orientada a Objetos - Clases - Heredando de una Clase

--- js/app.js
+++ js/app.js
@@ -8,7 +8,22 @@ class Tarea {
   }
 }
 
+class ComprasPendientes extends Tarea {
+    constructor(nombre, prioridad, cantidad) {
+        super(nombre, prioridad);
+        this.cantidad = cantidad;
+    }
+    mostrar() {
+        return `${super.mostrar()} y la cantidad de ${this.cantidad}`;
+    }
+}
+
 let tarea1 = new Tarea('Aprendiendo JavaScript', 'Alta');
 
 console.log(tarea1);
 console.log(tarea1.mostrar());
+
+let compra1 = new ComprasPendientes('Jabon', 'Alta', 3);
+
+console.log(compra1);
+console.log(compra1.mostrar());
