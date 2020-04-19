dd870ed9e24b29391ffc65f29ec5ec06bc78ca4e
Matias Gomez <jmgametal@gmail.com>
2020-04-19 18:46:35 -0300

    Seccion 4: Introducción y Básicos de React / Clase 36: Básicos de JSX

--- proyectos/basicos/src/components/Header.jsx
+++ proyectos/basicos/src/components/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'; // Este import siempre hay que ponerlo para tener acceso a todos lo metodos y funciones de React
 
 function Header() {
+  const edad = 18;
+  let mensaje;
+
+  if ( edad >= 18) {
+    mensaje = 'Eres mayor de edad.';
+  } else {
+    mensaje = 'Eres menor de edad.';
+  }
 
   return (
-    <h1>Desde el header</h1>
+    <div>
+      <h1>{mensaje} Tu edad es de {edad} años</h1>
+      <h1 className="encabezado">Tienda Virtual</h1>
+    </div>
+    // Para usar variables declaras antes del 'return' se usn entre {}
+    // Para definir una clase de estilo hay que usar 'className' ya ue la palabra 'class' es una palabra reservada de JavaSript
   )
 }
 
