ddfd3011d24826e450ec6dac81a200a66eee8af3
Matias Gomez <jmgametal@gmail.com>
2020-04-19 15:19:29 -0300

    Seccion 2: JavaScript - Repaso / Clase 28: Programación Orientada a Objetos - Clases

--- js/app.js
+++ js/app.js
@@ -1,34 +1,14 @@
-// Manera de hacerlo en 2020
-async function getUserAsync(cantidadUsuarios) {
-  const api = `https://randomuser.me/api/?results=${cantidadUsuarios}&nat=us`;
-
-  try {
-    let response = await fetch(api);
-    let data = await response.json();
-    return data.results;
-  } catch (error) {
-    return `Ocurrio el siguiente error:  ${error}`;
+class Tarea {
+  constructor(nombre, prioridad) {
+    this.nombre = nombre;
+    this.prioridad = prioridad;
+  }
+  mostrar() {
+    return `La tarea ${this.nombre} tiene una prioridad ${this.prioridad}`;
   }
 }
 
-getUserAsync(20).then((data) => {
-  imprimirHTML(data);
-});
+let tarea1 = new Tarea('Aprendiendo JavaScript', 'Alta');
 
-function imprimirHTML(usuarios) {
-    let html = '';
-
-    usuarios.forEach(usuario => {
-        html += `
-            <li>
-                Nombre: ${usuario.name.first} ${usuario.name.last}
-                País: ${usuario.nat}
-                Imagen:
-                    <img src="${usuario.picture.medium}">
-            </li>`;
-    });
-
-    const appContenedor = document.querySelector('#app');
-
-    appContenedor.innerHTML = html;
-}
+console.log(tarea1);
+console.log(tarea1.mostrar());
