7c24e5368c3acc83a2075f2fad0d1d54f4a844cb
Matias Gomez <jmgametal@gmail.com>
2020-04-19 18:28:45 -0300

    Seccion 4: Introducción y Básicos de React / Clase 35: Creando nuestro primer Componente

--- proyectos/basicos/src/App.js
+++ proyectos/basicos/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import Header from './components/Header'
 
-function App() {
+function App(){
+
   return (
     <div className="App">
-
-      <h1>Hola Mundo!!</h1>
-      
+      <Header />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ proyectos/basicos/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; // Este import siempre hay que ponerlo para tener acceso a todos lo metodos y funciones de React
+
+function Header() {
+
+  return (
+    <h1>Desde el header</h1>
+  )
+}
+
+export default Header;
