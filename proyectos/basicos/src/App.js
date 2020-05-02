5c697a4dbf082e15a4300215f4d2c496d5d6b6fe
Matias Gomez <jmgametal@gmail.com>
2020-05-02 15:43:01 -0300

    Seccion 4: Introducción y Básicos de React / Clase 40: Primeros pasos con el State de React

--- proyectos/basicos/src/App.js
+++ proyectos/basicos/src/App.js
@@ -1,9 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-function App() {
+// El state es todo lo que va a reacionar a una accion del usuario
 
+function App() {
+  // Crear listado de productos
+  const [ productos, saveProductos ] = useState([
+    { id: 1, nombre: 'Camisa ReactJS', precio: 50 },
+    { id: 2, nombre: 'Camisa VueJS', precio: 40 },
+    { id: 3, nombre: 'Camisa Node.js', precio: 20 },
+    { id: 4, nombre: 'Camisa Angular', precio: 30 },
+  ])
   // Obtener fecha actual
   const fecha = new Date().getFullYear(); 
 
