3d583a50b6851639020442f0be604ec425cd1311
Matias Gomez <jmgametal@gmail.com>
2020-05-02 16:40:39 -0300

    Seccion 4: Introducción y Básicos de React / Clase 42: Eventos en React

--- proyectos/basicos/src/components/Producto.js
+++ proyectos/basicos/src/components/Producto.js
@@ -3,11 +3,20 @@ import React from 'react'; // Escribir imr y tab y autocompleta
 const Producto = ({ producto }) => {
   const {id, nombre, precio} = producto;
 
+  // Agregar producto al carrito
+  const sleccionarProducto = id=> {
+    console.log('comprando... ', id);
+  }
   return ( 
   
     <div>
       <h3>{nombre}</h3>
       <p>${precio}</p>
+      <button
+        type="button"
+        onClick={ () => sleccionarProducto(id) }>
+        Comprar
+      </button>
     </div>
   );
 }
