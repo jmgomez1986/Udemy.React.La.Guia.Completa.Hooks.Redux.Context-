7e1471a59f878a9665f47b5905d244af0ab78c28
Matias Gomez <jmgametal@gmail.com>
2020-05-16 17:16:23 -0300

    Seccion 5: PROYECTO: Administrador de Pacientes / Clase 49: Creando el Componente de Formulario

--- proyectos/02_administrador_pacientes/src/App.js
+++ proyectos/02_administrador_pacientes/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Formulario from './components/Formulaio'
 
 function App() {
   return (
-    <h1>Adminisrador de pacientes</h1>
+    <Fragment>
+      <h1>Adminisrador de pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">
+
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
