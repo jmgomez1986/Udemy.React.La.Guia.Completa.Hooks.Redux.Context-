a9ea7fb01503dc02d992a2262183252258003d4f
Matias Gomez <jmgametal@gmail.com>
2020-05-16 21:32:12 -0300

    Seccion 5: PROYECTO: Administrador de Pacientes / Clase 50: Agregando los campos del formulario

--- proyectos/02_administrador_pacientes/src/components/Formulaio.js
+++ proyectos/02_administrador_pacientes/src/components/Formulaio.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Formulario = () => {
   return (
-    <h1>Desde formulario</h1>
+    <Fragment>
+      <h2>Crear cita</h2>
+
+      <form>
+        <label>Nombre mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+        />
+
+        <label>Nombre del dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre del Dueño de la Mascota"
+        />
+
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+        />
+
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+        />
+
+        <label>Síntomas</label>
+        <textarea
+          name="sintomas"
+          className="u-full-width">
+        </textarea>
+
+        <button
+          type="submit"
+          className="u-full-width button-primary">
+          Agrega cita
+        </button>                  
+      </form>
+    </Fragment>
   );
 }
 
