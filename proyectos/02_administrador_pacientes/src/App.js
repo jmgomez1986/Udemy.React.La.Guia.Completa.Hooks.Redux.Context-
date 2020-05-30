7e2878a5bae27a21fbf826d90da6b4cb611a793d
Matias Gomez <jmgametal@gmail.com>
2020-05-30 17:06:10 -0300

    Seccion 5: PROYECTO: Administrador de Pacientes / Clase 58: Mostrando las Citas

--- proyectos/02_administrador_pacientes/src/App.js
+++ proyectos/02_administrador_pacientes/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import Formulario from './components/Formulario'
+import Cita from './components/Cita';
 
 function App() {
 
@@ -24,7 +25,13 @@ const crearCita = cita => {
               crearCita={crearCita}/>
           </div>
           <div className="one-half column">
-
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+              />
+            ))}
           </div>
         </div>
       </div>
