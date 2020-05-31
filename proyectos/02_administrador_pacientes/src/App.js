756cfb2f944b761593b7ac84735ca0bba73e1395
Matias Gomez <jmgametal@gmail.com>
2020-05-31 14:53:38 -0300

    Seccion 5: PROYECTO: Administrador de Pacientes / Clase 61: El hook useEffect

--- proyectos/02_administrador_pacientes/src/App.js
+++ proyectos/02_administrador_pacientes/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario'
 import Cita from './components/Cita';
 
@@ -6,6 +6,11 @@ function App() {
 
   // Arreglo de citas
   const [citas, guardarCitas] = useState([]);
+
+  //  Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect(() => {
+    console.log('Documento listoo algo paso con las citas');
+  }, [citas]);
 
   // Funcion que tome las citas actuales y agregue la nueva
   const crearCita = cita => {
