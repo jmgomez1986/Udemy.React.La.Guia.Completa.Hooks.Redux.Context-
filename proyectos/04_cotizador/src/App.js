068d978907f80936298069bb83a421ebf98a2ced
Matias Gomez <jmgametal@gmail.com>
2020-08-14 19:08:09 -0300

    Seccion 7: PROYECTO: Cotizador de Seguro de Automovil / Clase 86: Creando la App y Ajustes

---
+++ proyectos/04_cotizador/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header'
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header
+      titulo="Cotizador de seguros"
+    />
+    </div>
+  );
+}
+
+export default App;
