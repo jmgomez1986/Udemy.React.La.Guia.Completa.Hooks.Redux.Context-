537b5f721e6db251ce84d147cc2cf9a1a3fb4b2e
jmgomez1986 <jmgametal@gmail.com>
2020-08-22 17:59:27 -0300

    Seccion 7: PROYECTO: Cotizador de Seguro de Automovil / Clase 89: Creando el Formulario

--- proyectos/04_cotizador/src/App.js
+++ proyectos/04_cotizador/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import Header from './components/Header'
 import styled from '@emotion/styled';
+import Header from './components/Header'
+import Formulario from './components/Formulario'
 
 const Contenedor = styled.div`
   max-width: 600px;
   margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #ffffff;
+  padding: 3rem;
 `;
 
 function App() {
@@ -13,6 +19,13 @@ function App() {
       <Header
         titulo="Cotizador de seguros"
       />
+
+      <ContenedorFormulario>
+        <Formulario
+
+        />
+      </ContenedorFormulario>
+
     </Contenedor>
   );
 }
