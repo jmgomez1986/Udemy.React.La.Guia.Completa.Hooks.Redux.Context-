74b58c154f044ff5ae57fe720fdead5802d5e726
jmgomez1986 <jmgametal@gmail.com>
2020-08-22 17:14:38 -0300

    Seccion 7: PROYECTO: Cotizador de Seguro de Automovil / Clase 88: Creando los Primeros Styled Components

--- proyectos/04_cotizador/src/App.js
+++ proyectos/04_cotizador/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from './components/Header'
+import styled from '@emotion/styled';
 
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-    <Header
-      titulo="Cotizador de seguros"
-    />
-    </div>
+    <Contenedor>
+      <Header
+        titulo="Cotizador de seguros"
+      />
+    </Contenedor>
   );
 }
 
